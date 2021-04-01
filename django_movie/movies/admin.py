ab19deba51717f331653c08185fcfe36e7790a44
Sergey Kornienko <skornienko29@gmail.com>
2021-04-01 18:05:01 +0300

    add actions

--- django_movie/movies/admin.py
+++ django_movie/movies/admin.py
@@ -1,7 +1,23 @@
+from django import forms
 from django.contrib import admin
 
 # Register your models here.
+from django.utils.safestring import mark_safe
+
 from .models import Category, Genre, Movie, MovieShots, Actor, Rating, RatingStar, Reviews
+
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+
+class MovieAdminForm(forms.ModelForm):
+    description = forms.CharField(
+        label='Описание',
+        widget=CKEditorUploadingWidget(),
+    )
+
+    class Meta:
+        model = Movie
+        fields = '__all__'
 
 
 @admin.register(Category)
@@ -18,22 +34,35 @@ class ReviewInline(admin.TabularInline):
     readonly_fields = ('name', 'email')
 
 
+class MovieShotsInline(admin.TabularInline):
+    model = MovieShots
+    extra = 1
+    readonly_fields = ('get_image',)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width=40% height=auto')
+
+    get_image.short_description = "Изображение"
+
+
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     """Movies."""
     list_display = ('title', 'category', 'url', 'draft')
     list_filter = ('category', 'year')
     search_fields = ('title', 'category__name')
-    inlines = [ReviewInline]
+    inlines = [MovieShotsInline, ReviewInline]
     save_on_top = True
     save_as = True
     list_editable = ('draft',)
+    actions = ['publish', 'unpublish']
+    form = MovieAdminForm
     fieldsets = (
         (None, {
             'fields': (('title', 'tagline'),)
         }),
         (None, {
-            'fields': (('description', 'poster'),)
+            'fields': ('description', ('poster', 'get_image'))
         }),
         (None, {
             'fields': (('year', 'world_premiere', 'country'),)
@@ -49,6 +78,36 @@ class MovieAdmin(admin.ModelAdmin):
             'fields': (('url', 'draft'),)
         }),
     )
+    readonly_fields = ('get_image',)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.poster.url} width=100 height=auto')
+
+    def unpublish(self, request, queryset):
+        """Remove publication."""
+        row_update = queryset.update(draft=True)
+        if row_update == 1:
+            message_bit = '1 запись была обновлена'
+        else:
+            message_bit = f'{row_update} записей были обновлены'
+        self.message_user(request, f'{message_bit}')
+
+    def publish(self, request, queryset):
+        """Make publication."""
+        row_update = queryset.update(draft=False)
+        if row_update == 1:
+            message_bit = '1 запись была обновлена'
+        else:
+            message_bit = f'{row_update} записей были обновлены'
+        self.message_user(request, f'{message_bit}')
+
+    publish.short_description = 'Опубликовать'
+    publish.allowed_permissions = ('change', )
+
+    unpublish.short_description = 'Снять с публикации'
+    unpublish.allowed_permissions = ('change', )
+
+    get_image.short_description = "Постер"
 
 
 @admin.register(Reviews)
@@ -67,7 +126,14 @@ class GenreAdmin(admin.ModelAdmin):
 @admin.register(Actor)
 class ActorAdmin(admin.ModelAdmin):
     """Actors."""
-    list_display = ('name', 'age')
+    list_display = ('name', 'age', "get_image")
+    readonly_fields = ('get_image',)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width=10% height=auto')
+
+    get_image.short_description = "Изображение"
+
 
 
 @admin.register(Rating)
@@ -79,7 +145,16 @@ class RatingAdmin(admin.ModelAdmin):
 @admin.register(MovieShots)
 class MovieShotsAdmin(admin.ModelAdmin):
     """MovieShots."""
-    list_display = ('title', 'movie')
+    list_display = ('title', 'movie', 'get_image')
+    readonly_fields = ('get_image',)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width=10% height=auto')
+
+    get_image.short_description = "Изображение"
 
 
 admin.site.register(RatingStar)
+
+admin.site.site_title = 'Django Movies'
+admin.site.site_header = 'Django Movies'
